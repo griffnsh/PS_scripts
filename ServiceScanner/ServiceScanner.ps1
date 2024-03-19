125979dff09851174fd424c94ad394932fce82d6
griff <memvivexp@gmail.com>
2024-03-19 09:45:05 -0500

    added logging

--- ServiceScanner/ServiceScanner.ps1
+++ ServiceScanner/ServiceScanner.ps1
@@ -9,18 +9,22 @@ foreach ($Service in $ServicesList) {
     if ($Service.Status -ne $CurrentServiceStatus) {
         $Log = "Service : $($Service.Name) is currently $CurrentServiceStatus, should be $($Service.Status)"
         Write-Output $Log
+        Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
 
         $Log = "Setting $($Service.Name) to $($Service.Status)"
         Write-Output $Log
+        Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
         Set-Service -Name $Service.Name -Status $Service.Status
 
         $AfterServiceStatus = (Get-Service -Name $Service.Name).Status
         if ($Service.Status -eq $AfterServiceStatus) {
             $Log = "Action was successful Service $($Service.Name) is now $AfterServiceStatus"
             Write-Output $Log
+            Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
         } else {
             $Log = "Action failed -- Service $($Service.Name) is now $AfterServiceStatus should be $($Service.Status)"
             Write-Output $Log
+            Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
         }
     }
 }
